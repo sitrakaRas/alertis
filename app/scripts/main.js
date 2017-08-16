15501789d7f5cd386cc42856b639299d44361956
sitraka <sitraka@bocasay.fr>
2017-08-16 22:23:42 +0300

    agrement et confiance

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,15 @@
 var alertis = {
-    
+    slider: function(parent,arg){
+        parent.slick(arg);
+    }
 }
+
+$(document).ready(function() {
+    if($(".slider-confiance").length > 0){
+        var arg = {
+            variableWidth: true
+
+        }
+        alertis.slider($(".slider-confiance"),arg);
+    }
+});
